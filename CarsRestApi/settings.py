ba8e025795e16cc5b9a62d3a84ee97008f96945d
rafal.rucki <rafalkrzysztofrucki@gmail.com>
2022-01-07 12:41:25 +0100

    Docker files added

--- CarsRestApi/settings.py
+++ CarsRestApi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '127.0.0.1', 'https://simple-car-rest-api.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
